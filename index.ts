00496cae23be5aec57b392287ca9884c4b2d8a48
muhammad nasir <muhammadnasir8787914@gmail.com>
2024-04-19 22:02:27 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,33 @@
+#! /usr/bin/env node
+
+
+console.log(`       ( Wellcome my word count Project )          `)
+
+
+import inquirer from "inquirer"
+
+const answers : {
+    Sentence :  string
+} = await inquirer.prompt(
+    
+    [
+
+        {
+            name : "Sentence",
+            type : "input",
+            message : "Enter your sentence to count the word:"
+
+
+
+        }
+
+]
+
+)
+
+const words = answers.Sentence.trim().split(" ")
+
+console.log(words)
+
+console.log(`your sentence word count is: ${words.length}`)
+
